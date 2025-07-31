14bf8708ecfffc1262e44e75ff5045b5979b78b0
Nishan Neupane <nishanneupane368@gmail.com>
2025-07-31 23:55:06 +0545

    update : hometution designs

---
+++ prisma/seed.ts
@@ -0,0 +1,28 @@
+const { PrismaClient } = require("@prisma/client");
+
+const prisma = new PrismaClient();
+
+async function main() {
+  await prisma.admin.upsert({
+    where: { email: "admin@hrhometuition.com" },
+    update: {},
+    create: {
+      id: "admin_001",
+      email: "admin@hrhometuition.com",
+      password: "$2a$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi", // password
+      name: "System Administrator",
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+  });
+}
+
+main()
+  .then(() => {
+    console.log("✅ Seed successful");
+    return prisma.$disconnect();
+  })
+  .catch((e) => {
+    console.error("❌ Seed failed", e);
+    return prisma.$disconnect().finally(() => process.exit(1));
+  });
